eba4495d06ea638b2ca6f74ccfd79e3eee7ebfbc
oscar9 <omarti87@gmail.com>
2014-07-16 13:09:02 +0200

    no path need

--- src/test_import_gvpy.py
+++ src/test_import_gvpy.py
@@ -4,15 +4,16 @@ import gvsig
 import gvsig_raster
 
 def main(*args):
-    #You just need to change the param PATH for one that exist in your computer
-    
+
     #Generate random raster inside the analysis extent
     #can set cellsizeo of the raster result
+    #if you want choose where to save the shape, you should add one attribute like this:
+    #     ,PATH="C://gvsig//random_shape.shp",
     r = gvpy.runalg("generaterandomnormal", EXTENT = [0,0,0,500,500,0], CELLSIZE=10, MEAN =0.5, STDDEV = 0.5)
 
     #Change the orientation of the grid
     #we can set the path
-    r1 = gvpy.runalg("gridorientation",r,0, PATH = "C://gvsig//Grid_orientation.tif")
+    r1 = gvpy.runalg("gridorientation",r,0)
 
     #Gradient lines (vector) 
     #as input we introduce the result of the last geoprocess r1
@@ -22,7 +23,8 @@ def main(*args):
     #we don't need to write parameters name as we introduce them in order
     # and if the algorithm generates 2 or more layers as result, we will pick them as list
     # if is just one, as unique object
-    r3 = gvpy.runalg("cva", r1, r1, r1, r1, PATH=["C:/gvsig/1.tif","C:/gvsig/2.tif"], CELLSIZE=100)
+    #Also, you can specify both names with: , PATH=["C:/gvsig/1.tif","C:/gvsig/2.tif"]
+    r3 = gvpy.runalg("cva", r1, r1, r1, r1, CELLSIZE=100)
     print "List layers result:",r3[0], r3[1]
 
     #You can check the orden of the params writing this here or in Jython console
@@ -35,7 +37,7 @@ def main(*args):
     #also we can introduce a layer as Analysis Extent
     #doesnt matter if is: vector, raster, gvsig.currentView(), gvsig.currentLayer() or
     #coordinates of corners [0,0,500,500]
-    v = gvpy.runalg("randomvector", 20, 1, EXTENT=r1, PATH = "C://gvsig//random_vector.shp")
+    v = gvpy.runalg("randomvector", 20, 1, EXTENT=r1)
 
     #We don't need to write "v=" before the algorithm if we don't want take the layer result
     gvpy.runalg("gvSIG-convexhull", v, True)
